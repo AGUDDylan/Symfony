f20c77601eaec59878b160e67b64872baf8d9341
dylan <aguddylan@gmail.com>
2017-03-08 15:42:43 +0100

    Tri des 10 requetes SQL

--- src/AppBundle/Repository/TweetRepository.php
+++ src/AppBundle/Repository/TweetRepository.php
@@ -10,4 +10,17 @@ namespace AppBundle\Repository;
  */
 class TweetRepository extends \Doctrine\ORM\EntityRepository
 {
+
+
+
+    public function  getLastTweets($maxResults = 10)
+    {
+
+        return $this->createQueryBuilder('t')
+            ->select('t')
+            ->orderBy('t.createdAt','DESC')
+            ->setMaxResults($maxResults)
+            ->getQuery()
+            ->getResult();
+    }
 }
